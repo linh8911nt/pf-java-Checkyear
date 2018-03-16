69a86c78e82eadc06504582e8626ab3942b308bf
linh8911nt <thanhlinh12314@gmail.com>
2018-03-16 16:11:55 +0700

    pf-java-Checkyear

---
+++ PfJavaCheckyear.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PfJavaCheckyear {
+    public static void main(String[] args) {
+        int year;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập năm:");
+        year = scanner.nextInt();
+
+        if (year%4 == 0){
+            if (year%100 == 0){
+                if (year%400 == 0){
+                    System.out.printf("%d Là năm nhuận",year);
+                }else {
+                    System.out.printf("%d Không là năm nhuận",year);
+                }
+            }else {
+                System.out.printf("%d Là năm nhuận",year);
+            }
+
+        }else {
+            System.out.printf("%d Không là năm nhuận",year);
+        }
+    }
+}
